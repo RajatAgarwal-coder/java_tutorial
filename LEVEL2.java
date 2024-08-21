e95e46ab2373c97384fb5492e57df6a5f0e8af9f
RajatAgarwal-coder <rajatagarwal061207@gmail.com>
2024-08-21 17:38:34 +0530

    To add the JAVA Source Code File of LEVEL2

---
+++ LEVEL2.java
@@ -0,0 +1,49 @@
+import java.lang.*;
+
+public class LEVEL2 {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+
+        System.out.print("First");
+        System.out.print("Second");  // Output of this are printed in same line
+        System.out.println("Third");
+
+        System.out.println();  // It will create a blank line
+
+        System.out.println("First");
+        System.out.println("Second");  // Output of this are printed in next line
+        System.out.println("Third");  // println add the line after text
+
+        // Programming Challenges
+
+        // Challenge 1
+        System.out.println("Good Morning");
+
+        // Challenge 2
+        System.out.println("Subscribe");
+
+        // Challenge 3
+        System.out.println("*");
+        System.out.println("* *");
+        System.out.println("* * *");
+        System.out.println("* * * *");
+        System.out.println("* * * * *");
+
+        System.out.println();
+
+        System.out.println("* * * * *");
+        System.out.println("* * * *");
+        System.out.println("* * *");
+        System.out.println("* *");
+        System.out.println("*");
+
+        System.out.println();
+
+        System.out.println("        *");
+        System.out.println("      * *");
+        System.out.println("    * * *");
+        System.out.println("  * * * *");
+        System.out.println("* * * * *");        
+
+    }
+}
